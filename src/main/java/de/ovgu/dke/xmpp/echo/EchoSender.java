0f5451a2f0e308360db3972bb22caf51d3c405a1
Stefan Haun <stefan.haun@ovgu.de>
2012-02-03 20:05:43 +0000

    Use mocca context for sending command.
    git-svn-id: https://svn.dke-research.de/svn/hci/xmpp/trunk/Echo@93 877cf7fc-6040-4152-ad77-284d8c8eda15

--- src/main/java/de/ovgu/dke/xmpp/echo/EchoSender.java
+++ src/main/java/de/ovgu/dke/xmpp/echo/EchoSender.java
@@ -6,64 +6,57 @@ import java.util.Properties;
 
 import de.ovgu.dke.glue.api.serialization.SerializationProvider;
 import de.ovgu.dke.glue.api.transport.*;
-import de.ovgu.dke.glue.util.transport.ClosedPacketHandler;
 import de.ovgu.dke.mocca.MoccaException;
 import de.ovgu.dke.mocca.command.Command;
 import de.ovgu.dke.mocca.command.DefaultCommandFactory;
+import de.ovgu.dke.mocca.context.Context;
+import de.ovgu.dke.mocca.context.ContextImpl;
+import de.ovgu.dke.mocca.control.CommandHandler;
+import de.ovgu.dke.mocca.glue.CommandPacketHandlerFactory;
 import de.ovgu.dke.mocca.glue.CommandSerializationProvider;
 
 public class EchoSender {
-	public static void main(String[] args) throws TransportException,
-			MoccaException, IOException {
-		// initialize and register transport factory
-		final SerializationProvider serializers = CommandSerializationProvider
-				.getInstance();
-		TransportRegistry.getInstance().loadTransportFactory(
-				"de.ovgu.dke.glue.xmpp.transport.XMPPTransportFactory",
-				new ClosedPacketHandlerFactory(), serializers,
-				TransportRegistry.AS_DEFAULT, TransportRegistry.DEFAULT_KEY);
+    public static void main(String[] args) throws TransportException,
+            MoccaException, IOException {
+        // initialize and register transport factory
+        final SerializationProvider serializers = CommandSerializationProvider
+                .getInstance();
+        TransportRegistry.getInstance().loadTransportFactory(
+                "de.ovgu.dke.glue.xmpp.transport.XMPPTransportFactory",
+                new CommandPacketHandlerFactory(new EchoCommandHandler()),
+                serializers, TransportRegistry.AS_DEFAULT,
+                TransportRegistry.DEFAULT_KEY);
 
-		// create a command
-		final Properties props = new Properties();
-		props.put("text", "Hallo Welt!");
-		final Command cmd = new DefaultCommandFactory()
-				.createCommand(URI
-						.create("http://dke.ovgu.de/mocca/test/command/echo"),
-						props);
+        // create a context
+        final Context ctx = new ContextImpl();
+        ctx.connect(URI.create("xmpp:shaun@bison.cs.uni-magdeburg.de"));
 
-		// get a transport
-		final Transport xmpp = TransportRegistry.getDefaultTransportFactory()
-				.createTransport(
-						URI.create("xmpp:shaun@bison.cs.uni-magdeburg.de"));
-		xmpp.checkCapabilities();
+        // create a command
+        final Properties props = new Properties();
+        props.put("text", "Hallo Welt!");
+        final Command cmd = new DefaultCommandFactory()
+                .createCommand(URI
+                        .create("http://dke.ovgu.de/mocca/test/command/echo"),
+                        props);
 
-		// create a packet thread
-		final PacketThread thread = xmpp
-				.createThread(PacketThread.DEFAULT_HANDLER);
+        // send the command
+        ctx.sendCommand(cmd);
 
-		// send the command
-		thread.send(cmd, Packet.Priority.DEFAULT);
+        System.in.read();
 
-		System.in.read();
+        // disconnect the context
+        ctx.disconnect();
 
-		// finish thread
-		thread.dispose();
+        // dispose the transport factory
+        TransportRegistry.getInstance().disposeAll();
 
-		// dispose the transport factory
-		TransportRegistry.getInstance().disposeAll();
-
-	}
+    }
 }
 
-class ClosedPacketHandlerFactory implements PacketHandlerFactory {
-	private static PacketHandler closedHandler = null;
+class EchoCommandHandler implements CommandHandler {
 
-	@Override
-	public synchronized PacketHandler createPacketHandler()
-			throws InstantiationException {
-		if (closedHandler == null)
-			closedHandler = ClosedPacketHandler.instance();
-
-		return closedHandler;
-	}
+    @Override
+    public void handleCommand(Command cmd, Context ctx) {
+        System.out.println("Received command: " + cmd.getCommand());
+    }
 }
