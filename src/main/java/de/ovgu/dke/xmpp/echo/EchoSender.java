7292c2bfb8c29ade1ae7f49ba708272bb6cc2e61
Stefan Haun <stefan.haun@ovgu.de>
2012-04-11 12:35:51 +0000

    Adapted to GLUE API changes (Context.getRuntime)
    git-svn-id: https://svn.dke-research.de/svn/hci/xmpp/trunk/Echo@111 877cf7fc-6040-4152-ad77-284d8c8eda15

--- src/main/java/de/ovgu/dke/xmpp/echo/EchoSender.java
+++ src/main/java/de/ovgu/dke/xmpp/echo/EchoSender.java
@@ -14,71 +14,71 @@ import de.ovgu.dke.mocca.api.context.Context;
 import de.ovgu.dke.mocca.util.MoccaHelper;
 
 public class EchoSender {
-    public static final URI echoCmd = URI
-            .create("http://dke.ovgu.de/mocca/test/command/echo");
+	public static final URI echoCmd = URI
+			.create("http://dke.ovgu.de/mocca/test/command/echo");
 
-    public static void main(String[] args) throws MoccaException, IOException {
+	public static void main(String[] args) throws MoccaException, IOException {
 
-        final MoccaRuntime mocca = MoccaHelper.getDefaultRuntime();
+		final MoccaRuntime mocca = MoccaHelper.getDefaultRuntime();
 
-        mocca.init();
+		mocca.init();
 
-        mocca.getCommandHandlerRegistry().registerCommandHandler(
-                new EchoCommandHandler());
+		mocca.getCommandHandlerRegistry().registerCommandHandler(
+				new EchoCommandHandler());
 
-        // create a context
-        final Context ctx = mocca.createContext();
-        ctx.connect(URI.create("xmpp:shaun@bison.cs.uni-magdeburg.de"));
+		// create a context
+		final Context ctx = mocca.createContext();
+		ctx.connect(URI.create("xmpp:shaun@bison.cs.uni-magdeburg.de"));
 
-        // create a command
-        final Properties props = new Properties();
-        props.put("text", "Hallo Welt!");
-        final Command cmd = mocca.createCommand(EchoCommandHandler.CMD_ECHO,
-                props);
+		// create a command
+		final Properties props = new Properties();
+		props.put("text", "Hallo Welt!");
+		final Command cmd = mocca.createCommand(EchoCommandHandler.CMD_ECHO,
+				props);
 
-        // send the command
-        ctx.sendCommand(cmd);
+		// send the command
+		ctx.sendCommand(cmd);
 
-        System.in.read();
+		System.in.read();
 
-        // disconnect the context
-        ctx.disconnect();
+		// disconnect the context
+		ctx.disconnect();
 
-        mocca.dispose();
-    }
+		mocca.dispose();
+	}
 }
 
 class EchoCommandHandler implements CommandHandler {
-    public static final URI CMD_ECHO = URI
-            .create("http://dke.ovgu.de/mocca/test/command/echo");
+	public static final URI CMD_ECHO = URI
+			.create("http://dke.ovgu.de/mocca/test/command/echo");
 
-    @Override
-    public Collection<URI> getAvailableCommands() throws MoccaException {
-        return Collections.singletonList(CMD_ECHO);
-    }
+	@Override
+	public Collection<URI> getAvailableCommands() throws MoccaException {
+		return Collections.singletonList(CMD_ECHO);
+	}
 
-    @Override
-    public void handleCommand(Command cmd, Context ctx) throws MoccaException {
-        final String ctx_id = (String) ctx.getAttribute(Context.ATTR_ID);
-        System.out.println("Received command " + cmd.getCommand()
-                + " on context " + ctx_id);
+	@Override
+	public void handleCommand(Command cmd, Context ctx) throws MoccaException {
+		final String ctx_id = (String) ctx.getAttribute(Context.ATTR_ID);
+		System.out.println("Received command " + cmd.getCommand()
+				+ " on context " + ctx_id);
 
-        final String text = cmd.getParameter("text");
-        System.out.println("Text is: " + text);
+		final String text = cmd.getParameter("text");
+		System.out.println("Text is: " + text);
 
-        Integer iteration = (Integer) ctx.getAttribute("iteration");
-        if (iteration == null)
-            iteration = new Integer(0);
-        ++iteration;
+		Integer iteration = (Integer) ctx.getAttribute("iteration");
+		if (iteration == null)
+			iteration = new Integer(0);
+		++iteration;
 
-        ctx.putAttribute("iteration", iteration);
+		ctx.putAttribute("iteration", iteration);
 
-        final Properties props = new Properties();
-        props.put("text", text);
-        props.put("iteration", Integer.toString(iteration));
-        final Command response = MoccaHelper.getDefaultRuntime().createCommand(
-                EchoSender.echoCmd, props);
+		final Properties props = new Properties();
+		props.put("text", text);
+		props.put("iteration", Integer.toString(iteration));
+		final Command response = ctx.getRuntime().createCommand(
+				EchoSender.echoCmd, props);
 
-        ctx.sendCommand(response);
-    }
+		ctx.sendCommand(response);
+	}
 }
